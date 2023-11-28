76d07dad47150f9bbfe648d50da9b0c76f4839a9
shun <samesamejinbei@gmail.com>
2023-11-28 20:12:02 +0900

    微修正

--- src/main/java/jp/co/sss/sns/controller/comment/CommentRegistController.java
+++ src/main/java/jp/co/sss/sns/controller/comment/CommentRegistController.java
@@ -6,6 +6,7 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.Date;
 import java.util.List;
+import java.util.Scanner;
 
 import javax.validation.Valid;
 
@@ -15,27 +16,25 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import jp.co.sss.sns.entity.Comment;
 import jp.co.sss.sns.form.CommentForm;
 import jp.co.sss.sns.repository.CommentRepository;
 
-
 @Controller
 public class CommentRegistController {
 	// DI
 	@Autowired
 	CommentRepository commentRepository;
-	
+
 	// コメントページ遷移処理
 	@RequestMapping("/snssns/commentPage")
 	public String commentPage(Model model, @ModelAttribute CommentForm form) {
 		return "comment/comment_page";
 	}
 
-	// コメント機能 
+	// コメント機能
 	@RequestMapping("/snssns/commentInput")
 	public String commentInput(Model model, @Valid @ModelAttribute CommentForm form, BindingResult result) {
 		if (result.hasErrors()) {
@@ -62,10 +61,10 @@ public class CommentRegistController {
 
 		// 入力値をリポジトリ保存
 		comment.setCommentContents(form.getCommentContents());
-		//投稿時間の取得
-		 SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd HH:mm");
-		 Date date = new Date();
-		 //SimpleDateFormatクラスのparseメソッドを使うにはthrows句を使ってParseExceptionなどに例外を投げるか、try-catch構文で例外処理を行う必要
+		// 投稿時間の取得
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd HH:mm");
+		Date date = new Date();
+		// SimpleDateFormatクラスのparseメソッドを使うにはthrows句を使ってParseExceptionなどに例外を投げるか、try-catch構文で例外処理を行う必要
 		try {
 			date = sdf.parse(DateTimeFormatter.ofPattern("yyyyMMddHHmmss").format(LocalDateTime.now()));
 		} catch (ParseException e) {
@@ -82,14 +81,72 @@ public class CommentRegistController {
 		return "comment/comment_complete";
 	}
 
-	//コメント数カウント処理
+	// コメント数カウント処理
 	@RequestMapping("/snssns/commentCount")
 	public String commentCount(int commentId) {
 		Comment commentContents = new Comment();
 		commentContents.setCommentId(commentId);
+
+	
+		try (Scanner sc = new Scanner(System.in)) {
+			int N;
+			int M;
+			String A;
+			int i; // iは (1 ≦ i ≦ N)
+			int G=0;
+			{
+				N = sc.nextInt();
+				M = sc.nextInt();
+				i = N - 1;
+				if (1 <= i && i <= N - 1) {
+//			最後の行読み取り
+					String L = sc.nextLine();
+					G = N + M;
+//					A_iの最後まで読み取りのため繰り返し
+					for (; i <= N - 1; i++) {
+						A = sc.next();
+						G = G +Integer.parseInt(A);
+
+					}
+					int A_i = N * i;
+					if (1 <= N && N <= 1000 && 1 <= M && M <= 100 && 1 <= A_i && A_i <= 200)
+					{
+//            表示処理
+						System.out.println(G+"\n");
+					}
+				}
+			}
+//            System.out.println("hello = " + token1 + " , world = " + token2);
+		}
+//ここまで
+		try (Scanner sc = new Scanner(System.in)) {
+		String S;
+		String KL = "+";
+		String KR= "+";
+		int SK;
+//		入力された内容
+		S=sc.nextLine();
+		SK = S.length();
+//		K= String.valueOf(SK * "+") ;
+// 		int KL2= Integer.parseInt(KL);
+//		for(int i =1; i<=SK+2;i++) {
+// 		System.out.println(KL2 * SK);
+// 		String str = StringUtils.repeat(S, SK);
+		
+//		K= String.valueOf(SK * "+") ;
+		int KL2= Integer.parseInt(KL);
+//		for(int i =1; i<=SK+2;i++) {
+		System.out.println(KL2 * SK);
+//		}
+		System.out.println("\n");
+		System.out.println(KL+S+KR+"\n");
+//		for(int i =1; i<=SK+2;i++) {
+		System.out.println(KL2 * SK);
+//			}
+		
+		}//tryの
+		
 		return "comment/comment_complete";
 	}
-
-
 
 }
