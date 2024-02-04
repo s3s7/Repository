b7876185987f0fab5f257bd88d49fe1ea6d68df8
shun <samesamejinbei@gmail.com>
2024-02-04 13:13:51 +0900

    コメント多い順に並べ替え実装中 #3

--- src/main/java/jp/co/sss/sns/repository/PostingRepository.java
+++ src/main/java/jp/co/sss/sns/repository/PostingRepository.java
@@ -20,6 +20,8 @@ public interface PostingRepository extends JpaRepository<Posting, Integer> {
 	//商品名検索（新着順）
 //	public List<Posting> findByNameLikeOrderByInsertDate(String name);
 
+	
+	
 //	default String[] findAllDate(String date) {
 //		List<String> list = new ArrayList<String>();
 //		Object[] datesObj = getFindDate(date);
@@ -31,20 +33,20 @@ public interface PostingRepository extends JpaRepository<Posting, Integer> {
 //		}
 //		return months;
 //}
-//		// 商品情報を売れ筋順で検索
+//		// 売れ筋順で検索
 //		@Query(value = JPQLConstant.FIND_ALL_ITEMS_ORDER_BY_ORDERNUM, nativeQuery = true)
 //		public Page<Item> findByDeleteFlagOrderByOrderNumDesc(Pageable pageable);
 //
-//		// 商品情報を新着順で検索（カテゴリ検索）
+//		// 新着順で検索（カテゴリ検索）
 //		public Page<Item> findByCategoryAndDeleteFlagOrderByInsertDateDesc(Category category,int deleteFlag, Pageable pageable);
 //
-//		// 商品情報を売れ筋順で検索（カテゴリ検索）
+//		// 売れ筋順で検索（カテゴリ検索）
 //		@Query(value = JPQLConstant.FIND_ITEMS_BY_CATEGORIES_ORDER_BY_QUANTITY, nativeQuery = true)
 //		public Page<Item> findByCategoryAndDeleteFlagOrderByOrderNumDesc(Integer categoryId, Pageable pageable);
 //
 	
 //
-//		//商品名検索（売れ筋順）
+//		//品名検索（売れ筋順）
 //		@Query(value = JPQLConstant.FIND_ITEMS_BY_ITEMS_ORDER_BY_QUANTITY, nativeQuery = true)
 //		public Page<Item> findByNameLikeAndDeleteFlagOrderByOrderNumDesc(String name,Pageable pageable);
 //	
