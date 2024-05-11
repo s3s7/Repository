abcd4a8fd574672965184de8e9962f0d73298676
shun <samesamejinbei@gmail.com>
2024-05-11 11:37:15 +0900

    feat:Line通知機能実装中 #9

--- src/main/java/jp/co/sss/sns/controller/line/LineNotificationController.java
+++ src/main/java/jp/co/sss/sns/controller/line/LineNotificationController.java
@@ -1,49 +1,56 @@
-package jp.co.sss.sns.controller.line;
-
-
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Controller;
-
-@RequiredArgsConstructor
-@Controller
-public class LineNotificationController {
-
-
-    public static void main(String[] args) {
-        LineNotify lineNotify = new LineNotify("x"); // LINE Notifyのアクセストークン(適宜変更)
-        lineNotify.notify("http://kakedashi-xx.comを閲覧することで、IT知識が拡充されます。");
-        System.out.println("javaからlineへ通知しました。");
-    }
-
-    private final String token;
-    public LineNotify(String token) {
-        this.token = token;
-    }
-
-    public void notify(String message) {
-        HttpURLConnection connection = null;
-        try {
-            URL url = new URL("https://notify-api.line.me/api/notify");
-            connection = (HttpURLConnection) url.openConnection();
-            connection.setDoOutput(true);
-            connection.setRequestMethod("POST");
-            connection.addRequestProperty("Authorization", "Bearer " + token);
-            try (OutputStream os = connection.getOutputStream();
-                 PrintWriter writer = new PrintWriter(os)) {
-                writer.append("message=").append(URLEncoder.encode(message, "UTF-8")).flush();
-                try (InputStream is = connection.getInputStream();
-                     BufferedReader r = new BufferedReader(new InputStreamReader(is))) {
-                    String res = r.lines().collect(Collectors.joining());
-                    if (!res.contains("\"message\":\"ok\"")) {
-                        System.out.println(res);
-                    }
-                }
-            }
-        } catch (Exception ignore) {
-        } finally {
-            if (connection != null) {
-                connection.disconnect();
-            }
-        }
-    }
-}
+//package jp.co.sss.sns.controller.line;
+//
+//import java.io.*;
+//import java.net.HttpURLConnection;
+//import java.net.URL;
+//import java.net.URLEncoder;
+//import java.util.Objects;
+//import java.util.stream.Collectors;
+//
+//
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.stereotype.Controller;
+//
+//@RequiredArgsConstructor
+//@Controller
+//public class LineNotificationController {
+//
+//
+//    public static void notify{
+//        LineNotify lineNotify = new LineNotify("x"); // LINE Notifyのアクセストークン(適宜変更)
+//        lineNotify.notify("http://kakedashi-xx.comを閲覧することで、IT知識が拡充されます。");
+//        System.out.println("javaからlineへ通知しました。");
+//    }
+//
+//    private final String token;
+//    public LineNotificationController(String token) {
+//        this.token = token;
+//    }
+//
+//    public void notify(String message) {
+//        HttpURLConnection connection = null;
+//        try {
+//            URL url = new URL("https://notify-api.line.me/api/notify");
+//            connection = (HttpURLConnection) url.openConnection();
+//            connection.setDoOutput(true);
+//            connection.setRequestMethod("POST");
+//            connection.addRequestProperty("Authorization", "Bearer " + token);
+//            try (OutputStream os = connection.getOutputStream();
+//                 PrintWriter writer = new PrintWriter(os)) {
+//                writer.append("message=").append(URLEncoder.encode(message, "UTF-8")).flush();
+//                try (InputStream is = connection.getInputStream();
+//                     BufferedReader r = new BufferedReader(new InputStreamReader(is))) {
+//                    String res = r.lines().collect(Collectors.joining());
+//                    if (!res.contains("\"message\":\"ok\"")) {
+//                        System.out.println(res);
+//                    }
+//                }
+//            }
+//        } catch (Exception ignore) {
+//        } finally {
+//            if (connection != null) {
+//                connection.disconnect();
+//            }
+//        }
+//    }
+//}
