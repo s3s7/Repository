9a8f7fd7dc624a15656d1b741ef3aafbf151c74c
shun <samesamejinbei@gmail.com>
2024-05-20 20:48:06 +0900

    style:TailwindCSS追加中 #10

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+//     colors: {
+//          'blue': '#1fb6ff',}
+    extend: {},
+  },
+  plugins: [],
+}
+
